547da12b174d3ed6ae6269b9bf13ba0e0b265f95
manue1666 <utm23090708@utma.edu.mx>
2025-03-26 22:35:59 -0600

    index correction

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ dotenv.config(); // Carga las variables de entorno desde .env
 
 async function main() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Aplicación conectada a la base de datos");
         app.listen(4000, () => {
             console.log("Aplicación corriendo");
